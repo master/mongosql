8845251510ee6c95f8a268d7361a59e3a0656dea
Oleg Smirnov <oleg.smirnov@gmail.com>
2011-03-09 19:18:11 +0200

    JavaScript semantic (highly experimental)

--- src/mongosql_sem.erl
+++ src/mongosql_sem.erl
@@ -33,14 +33,14 @@ compile({update, Table, Assign, Where}) ->
 
 compile({selection, '*'}) -> [];
 compile({selection, Arg}) -> [{fields, Arg}];
-compile({orderby, Arg1}) -> [{orderby, Arg1}];
+compile({orderby, Arg}) -> [{orderby, Arg}];
 compile({limit, Arg}) -> [{limit, Arg}];
 compile({offset, Arg}) -> [{offset, Arg}];
 compile({assign, Arg1, Arg2}) -> {compile(Arg1), compile(Arg2)};
 
 compile({'and', Arg1, Arg2}) -> compile(Arg1) ++ compile(Arg2);
 compile({'or', Arg1, Arg2}) -> [{<<"$or">>, [compile(Arg1), compile(Arg2)]}];
-compile({'not', Arg1}) -> [{<<"$not">>, compile(Arg1)}];
+compile({'not', Arg}) -> [{<<"$not">>, compile(Arg)}];
 
 compile({'=', Arg1, Arg2}) -> [{compile(Arg1), compile(Arg2)}];
 compile({'>', Arg1, Arg2}) -> [{compile(Arg1), [{gt, compile(Arg2)}]}];
@@ -50,16 +50,16 @@ compile({'<=', Arg1, Arg2}) -> [{compile(Arg1), [{lte, compile(Arg2)}]}];
 compile({'<>', Arg1, Arg2}) -> [{compile(Arg1), [{ne, compile(Arg2)}]}];
 
 compile({between, Arg1, Arg2, Arg3}) -> 
-    [{compile(Arg1), [{gt, compile(Arg2)}, {lt, compile(Arg3)}]}];
+    [{compile(Arg1), [{gte, compile(Arg2)}, {lte, compile(Arg3)}]}];
 compile({notbetween, Arg1, Arg2, Arg3}) -> 
-    [{compile(Arg1), [{gt, compile(Arg3)}, {lt, compile(Arg2)}]}];
+    [{compile(Arg1), [{gte, compile(Arg3)}, {lte, compile(Arg2)}]}];
 
 compile({like, Arg1, Arg2}) -> [{compile(Arg1), [{regexp, compile(Arg2), ""}]}];
 %% @todo '$not' with '$regex' is not supported by MongoDB
 %% compile({notlike,_Arg1,_Arg2}) -> false; 
 
-compile({null, Arg1}) -> [{compile(Arg1), [{exists, false}]}];
-compile({notnull, Arg1}) -> [{compile(Arg1), [{exists, true}]}];
+compile({null, Arg}) -> [{compile(Arg), [{exists, false}]}];
+compile({notnull, Arg}) -> [{compile(Arg), [{exists, true}]}];
 
 compile({in, Arg1, Arg2}) -> [{compile(Arg1), [{in, compile(Arg2)}]}];
 compile({notin, Arg1, Arg2}) -> [{compile(Arg1), [{nin, compile(Arg2)}]}];
@@ -77,3 +77,110 @@ compile(Token) when is_bitstring(Token) -> Token;
 compile(Token) when is_list(Token) -> Token;
 
 compile(Token) -> {unknown_token, Token}.
+
+%% JavaScript semantic
+
+compile({delete, Table, Where}, js) -> 
+    flatten(["db.", compile(Table, js), ".delete(", compile({where, Where}, js), ")"]);
+
+compile({insert, Table, Fields, Values}, js) -> 
+    flatten(["db.", compile(Table, js), ".save(", 
+	     list_to_obj(lists:zip(compile(Fields, js), compile(Values, js))), ")"]);
+
+compile({select,_, {count}, {Table, Where,_OrderBy,_GroupBy,_Having,_Limit,_Offset}}, js) -> 
+    flatten(["db.", compile(Table, js), ".count(", compile({where, Where}, js), ")"]);
+
+compile({select,_, Fields, {Table, Where, OrderBy,_GroupBy,_Having, Limit, Offset}}, js) -> 
+    flatten(["db.", compile(Table, js), ".find(", compile({where, Where}, js), ")",
+	    compile(OrderBy), compile(Limit), compile(Offset)]);
+
+compile({update, Table, Assign, Where}, js) ->
+    flatten(["db.", compile(Table, js), ".update(", 
+	     compile({where, Where}, js), ", {'$set': ", compile(Assign, js), "}, false, true)"]);
+
+compile({where, Arg}, js) -> flatten(["{ $where: \"",  compile(Arg), "\" } )"]);
+
+compile({orderby, Arg}, js) ->
+    flatten([".sort(",  list_to_obj(compile(Arg, js)), ")"]);
+compile({limit, Arg}, js) -> 
+    flatten([".limit(",  compile(Arg, js), ")"]);
+compile({offset, Arg}, js) -> 
+    flatten([".skip(",  compile(Arg, js), ")"]);
+
+%%compile({assign, Arg1, Arg2}, js) -> {compile(Arg1), compile(Arg2)};
+
+compile({asc, Arg}, js) ->
+    {compile(Arg, js), 1};
+compile({desc, Arg}, js) ->
+    {compile(Arg, js), -1};
+
+compile({'and', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") and (", compile(Arg2, js), ")"]);
+compile({'or', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") or (", compile(Arg2, js), ")"]);
+compile({'not', Arg1}, js) -> 
+    flatten(["not (", compile(Arg1, js), ")"]);
+
+compile({'=', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") == (", compile(Arg2, js), ")"]);
+compile({'>', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") > (", compile(Arg2, js), ")"]);
+compile({'<', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") < (", compile(Arg2, js), ")"]);
+compile({'>=', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") >= (", compile(Arg2, js), ")"]);
+compile({'<=', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") <= (", compile(Arg2, js), ")"]);
+compile({'<>', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") != (", compile(Arg2, js), ")"]);
+
+compile({between, Arg1, Arg2, Arg3}, js) -> 
+    compile({'and', compile({'>=', Arg1, Arg2}, js), compile({'<=', Arg1, Arg3}, js)}, js);
+compile({notbetween, Arg1, Arg2, Arg3}, js) -> 
+    compile({'not', compile({between, Arg1, Arg2, Arg3}, js)}, js);
+
+compile({like, Arg1, Arg2}, js) -> 
+    compile({notnull, flatten([compile(Arg1, js), ".match(", compile(Arg2, js), ")"])}, js);
+compile({notlike, Arg1, Arg2}, js) ->
+    compile({'not', compile({like, Arg1, Arg2}, js)}, js);
+
+compile({null, Arg}, js) -> 
+    compile({'==', compile(Arg, js), "null"}, js);
+compile({notnull, Arg}, js) -> 
+    compile({'!=', compile(Arg, js), "null"}, js);
+
+compile({in, Arg1, Arg2}, js) -> 
+    flatten([compile(Arg1, js), " in ", compile(Arg2, js)]);
+compile({notin, Arg1, Arg2}, js) -> 
+    compile({'not', compile({in, Arg1, Arg2}, js)}, js);
+
+compile({'+', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") + (", compile(Arg2, js), ")"]);
+compile({'-', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") - (", compile(Arg2, js), ")"]);
+compile({'*', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") * (", compile(Arg2, js), ")"]);
+compile({'/', Arg1, Arg2}, js) -> 
+    flatten(["(", compile(Arg1, js), ") / (", compile(Arg2, js), ")"]);
+
+compile(nil, js) -> "";
+
+compile(Token, js) when is_atom(Token) -> 
+    "this." + atom_to_list(Token);
+compile(Token, js) when is_integer(Token) -> 
+    integer_to_list(Token);
+compile(Token, js) when is_bitstring(Token) ->
+    "\"" + bitstring_to_list(Token) + "\"";
+compile(Token, js) when is_list(Token) -> 
+    Token;
+
+compile(Token, js) -> 
+    {unknown_token, Token}.
+
+flatten(List) -> string:join(List, "").
+
+list_to_obj(List) when is_list(List) ->
+    Items = string:join(lists:map(fun(R) -> 
+					  flatten([element(1, R), ": ", element(2, R)])
+				  end, List), ", "),
+    flatten(["{", Items, "}"]).
